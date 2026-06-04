ea49f5deeddb7a4a3a666c054fd1ba27c486dcd8
RohanTabhane <rohan.t0287@gmail.com>
2026-06-04 14:55:21 +0530

    Add React App component

---
+++ octofit-tracker/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>OctoFit Tracker</h1>
+      <p>A social fitness app designed to help students stay active and compete with their peers</p>
+    </div>
+  )
+}
+
+export default App
